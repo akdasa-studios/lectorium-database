c5e70ff68342b73902531df4c3ce72b2ce566cc2
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-10-31 20:27:46 +0100

    feat: jwt token authentication

--- modules/bootstrap/scripts/000-configure-database.ts
+++ modules/bootstrap/scripts/000-configure-database.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+import fs from 'fs'
 import { Migration } from './Migration'
 
 export class ConfigureDatabase extends Migration {
@@ -8,13 +10,27 @@ export class ConfigureDatabase extends Migration {
   }
 
   async migrate(): Promise<void> {
+    const jwtPublicKeyPath = path.resolve(__dirname, '../../keys/jwt_public_key.pem');
+    const jwtPublicKey = fs.readFileSync(jwtPublicKeyPath, 'utf8');
+
     const keys = [
       { key: 'httpd/enable_cors', value: 'true' },
       { key: 'cors/origins',      value: '*' },
       { key: 'cors/credentials',  value: 'true' },
       { key: 'cors/headers',      value: 'accept, authorization, content-type, origin, referer' },
       { key: 'cors/methods',      value: 'GET, PUT, POST, HEAD, DELETE, OPTIONS' },
-
+      {
+        key: 'chttpd/admin_only_all_dbs',
+        value: 'false'
+      },
+      {
+        key: 'chttpd/authentication_handlers',
+        value: "{chttpd_auth, cookie_authentication_handler}, {chttpd_auth, jwt_authentication_handler}, {chttpd_auth, default_authentication_handler}"
+      },
+      {
+        key: 'jwt_keys/rsa:_default',
+        value: jwtPublicKey.replace(/\n/g, '\\n')
+      },
       { key: 'couchdb/single_node', value: 'true' },
     ]
 
